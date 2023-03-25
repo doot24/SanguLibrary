e2f71c587ee90dfe09f669d2aca08fab5b18e10b
Akaki Khurtsidze <akakikhurtsidze77@gmail.com>
2023-03-25 23:04:01 +0400

    fix/sort pipeline also make values not mandatory

--- API/src/routes/Notification.ts
+++ API/src/routes/Notification.ts
@@ -5,6 +5,7 @@ import { body, query, validationResult } from 'express-validator';
 import { IsAuthenticated } from "../utils/AuthGuards";
 
 import { NotificationMetaDataSchema } from "../schemas/NotificationSchema";
+import { PipelineStage } from "mongoose";
 
 router.get("/", IsAuthenticated, query("page").notEmpty().isNumeric(), query("pageSize").notEmpty().isNumeric(), (req: Request, res: Response) => {
     const errors = validationResult(req);
@@ -17,10 +18,8 @@ router.get("/", IsAuthenticated, query("page").notEmpty().isNumeric(), query("pa
     let startIndex = (page - 1) * pageSize;
     let endIndex = page * pageSize;
 
-    let readStatus : Number = Number(req.query.read) || 0;
-
-    //by default get all messages that are week old.
-    let timeSpan : Number = Number(req.query.time) || 604800000;
+    let readStatus : Number = Number(req.query.read);
+    let timeSpan : Number = Number(req.query.time);
 
     const startTime = new Date(Date.now() - Number(timeSpan)); 
     const startDate = startTime.getTime();  
@@ -28,56 +27,78 @@ router.get("/", IsAuthenticated, query("page").notEmpty().isNumeric(), query("pa
     const endDate = Date.now();
 
     // add pagination stages to the pipeline
-    const agg = [
-        {
-            '$match': {
-                'receiver': req.session.user.userid,
-                'read': readStatus
+    let pipeline : Array<PipelineStage> = [];
+
+    pipeline.push({ $match: { receiver: req.session.user.userid } });
+    if(req.query.read)
+    {
+        pipeline.push({ $match: { read: readStatus } });
+    }
+    pipeline.push({
+      $lookup: {
+        from: 'notifications',
+        localField: 'attachedNotification',
+        foreignField: '_id',
+        as: 'notification'
+      }
+    });
+    if(req.query.time)
+    {
+        pipeline.push({
+          $match: {
+            'notification.created': {
+              $gte: startDate,
+              $lte: endDate
             }
-        },
-        {
-            '$lookup': {
-                'from': 'notifications',
-                'localField': 'attachedNotification',
-                'foreignField': '_id',
-                'as': 'notification'
-            }
-        },
-        {
-            '$match': {
-                'notification.created': {
-                    '$gte': startDate,
-                    '$lte' : endDate
-                }
-            }
-        },
-        {
-            '$skip': startIndex
-        },
-        {
-            '$limit': pageSize
+          }
+        });
+    }
+    pipeline.push({$sort : {'notification.created': -1}});
+    pipeline.push({ $skip: startIndex });
+    pipeline.push({ $limit: pageSize });
+    pipeline.push({
+        $facet: {
+          totalCount: [
+            { $count: 'count' }
+          ],
+          results: [
+            { $skip: startIndex },
+            { $limit: pageSize }
+          ]
         }
-    ];
+    });
 
     // execute the aggregation pipeline with pagination stages
-    NotificationMetaDataSchema.aggregate(agg).then((results) => {
-        NotificationMetaDataSchema.countDocuments({ receiver: req.session.user.userid }).exec().then((totalNotifications) => {
+    NotificationMetaDataSchema.aggregate(pipeline).then((results) => {
+        if (results[0].results.length == 0) {
             return res.status(200).json({
                 status: "success",
-                notifications: results,
-                totalNotifications: totalNotifications,
-                totalPages: Math.ceil(totalNotifications / pageSize),
+                notifications: [],
+                totalNotifications: 0,
+                totalPages: 0,
                 currentPage: page,
-                hasPreviousPage: page > 1,
-                hasNextPage: endIndex < totalNotifications
+                hasPreviousPage: false,
+                hasNextPage: false
             });
+        }
+        const totalCount = results[0].totalCount[0].count;
+        const data = results[0].results;
+      
+        return res.status(200).json({
+          status: "success",
+          notifications: data,
+          totalNotifications: totalCount,
+          totalPages: Math.ceil(totalCount / pageSize),
+          currentPage: page,
+          hasPreviousPage: page > 1,
+          hasNextPage: endIndex < totalCount
         });
-    }).catch(() => {
+      }).catch(() => {
         res.status(400).json({
-            status: "fail",
-            message: "მოთხოვნის დამუშავება ვერ მოხერხდა!"
+          status: "fail",
+          message: "მოთხოვნის დამუშავება ვერ მოხერხდა!"
         });
-    });
+      });
 });
 
 router.post("/setread", IsAuthenticated, body("notificationid").notEmpty().isUUID(), (req: Request, res: Response) => {
