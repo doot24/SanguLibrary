e77986aff697810d654c126b7987d40b581880dd
Mate-max <72877699+Mate-max@users.noreply.github.com>
2023-03-27 20:08:57 +0400

    Added mobile design for notifications

--- Site/src/views/Notifications.vue
+++ Site/src/views/Notifications.vue
@@ -1,11 +1,11 @@
 <template>
   <hamburger @click="hamburgerActive = !hamburgerActive" />
   <headerBar />
-  <div class="container-fluid d-flex flex-column min-vh-100 bodyClass">
+  <div class="container-fluid d-flex flex-column d-none d-lg-block d-xl-block d-sm-none min-vh-100 py-5 bodyClass">
     
-    <div class="row bodyClass py-5">
-  <div class="row" style="margin-bottom: 10px; margin-left: 60px;">
+    <div class="row" style="margin-bottom: 10px; margin-left: 60px;">
     <h3 class="text-light mb-5">შეტყობინებები</h3>
+    <div class="row bodyClass ">
     <div class="col-md-5 col-xl-3 d-flex justify-content-between align-items-center">
       <button id="read" :class="{ 'smallButtonHighlight': selectedSection == 'read' }" class="smallButton p-3 flex-grow-2"
         v-on:click="setReadValue(1); selectedSection = 'read'">წაკითხული</button>
@@ -14,7 +14,7 @@
     </div>
     <div class="col-md-4 col-xl-3 mt-3 mt-md-0" style="margin-left: 400px;">
       <select id="time-dropdown" class="d-flex justify-content-between smallButton p-3 text-center">
-        <option value="year">ყველა</option>
+        <option value="year">დროის მიხედვით</option>
         <option value="week">ბოლო კვირა</option>
         <option value="month">ბოლო თვე</option>
         <option value="year">ბოლო წელი</option>
@@ -53,7 +53,66 @@
       </div>
     </div>
   </div>
- 
+  <!-- Begin for phone screen -->
+  <div class="container-fluid d-flex flex-column d-block d-sm-block d-md-none min-vh-100 py-2 bodyClass">
+    
+    <div class="container-fluid d-flex flex-column min-vh-100 bodyClass py-5">
+    <div class="container d-flex flex-wrap " style="width: 99%;">
+      <div class="col-12 col-md-8 col-lg-6">
+        <h2 class="text-light mb-5 text-center"> შეტყობინებები </h2>
+        <div class="row">
+          <div class="col-12 col-md-6 mb-3 mb-md-0">
+            <button id="read" :class="{ 'smallButtonHighlight': selectedSection == 'read' }" class="smallButton p-3 w-100"
+              v-on:click="setReadValue(1); selectedSection = 'read'">წაკითხული</button>
+          </div>
+          <div class="col-12 col-md-6">
+            <button :class="{ 'smallButtonHighlight': selectedSection == 'notread' }" class="smallButton p-3 w-100"
+              v-on:click="setReadValue(0); selectedSection = 'notread'">წაუკითხავი</button>
+          </div>
+          <div class="col-12 my-3">
+            <label for="time-dropdown" class="bi bi-clock text-light mb-3 d-block text-center"> დროის მიხედვით</label>
+            <select id="time-dropdown" class="d-flex smallButton p-3 w-100 text-center" @change="setByTime($event.target.value)">
+    <option value="week">ყველა</option>
+    <option value="week">ბოლო კვირა</option>
+    <option value="month">ბოლო თვე</option>
+    <option value="year">ბოლო წელი</option>
+</select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="mt-3 container-fluid vh-100 rounded d-flex flex-column gap-2 p-5"
+      style="background-color: #322E3D; width: 90%; overflow-y: scroll;">
+      <div v-for="(meta) in notifications" :key="meta._id"
+        v-on:click="this.selectedNotificationMeta = meta; this.selectedNotification = meta.notification[0]; setRead();"
+        data-bs-toggle="modal" data-bs-target="#notificationModals" class="d-flex flex-column gap-2">
+        <h3 class="text-light">{{ meta.notification[0].title }}</h3>
+        <span style="color: #716E6E;">{{ formatDate(meta.notification[0].created) }}</span>
+        <div class="table-border mt-3"></div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="notificationModals" tabindex="-1" aria-labelledby="notificationModalsLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content" style="background-color: #322E3D;">
+          <div class="modal-header">
+            <h5 class="modal-title" id="notificationModalsLabel" style="color:#D9D9D9">{{ selectedNotification.title }}
+            </h5>
+            <button type="button" class="btn-close"
+              v-on:click="this.selectedNotification = {}; this.selectedNotificationMeta = {}" data-bs-dismiss="modal"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body" style="color: #B3B3B3;">
+            <p>{{ selectedNotification.text }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <style scoped>
