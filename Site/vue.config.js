5e4713217be628198103da8d36226a56a9e27b89
doot24 <akakikhurtsidze77@gmail.com>
2023-03-31 10:18:26 +0400

    fix building issues on windows

--- Site/vue.config.js
+++ Site/vue.config.js
@@ -1,11 +1,13 @@
 const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   configureWebpack: {
     plugins: [
       new Dotenv({
         path: './.env'
-      })
+      }),
+      new MiniCssExtractPlugin()
     ],
     resolve: {
       
