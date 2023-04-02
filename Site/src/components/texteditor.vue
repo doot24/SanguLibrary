d5b1064997679c329fcbf297329482f20b88aaab
Mate-max <72877699+Mate-max@users.noreply.github.com>
2023-04-02 18:27:30 +0400

    Added some changes

--- Site/src/components/texteditor.vue
+++ Site/src/components/texteditor.vue
@@ -1,58 +1,71 @@
 <template>
-    <div class="container-background">
-    <QuillEditor class="editor-background" :style="{ height: `${Height}px` }" :content-type="'html'" v-model:content="contents" @update:content="updateContents" theme="snow"
-        :options="editorOptions"  />
-</div>
+  <div>
+    <button
+      @click="showEditor = true"
+      class="btn btn-light bi bi-pencil-square"
+    ></button>
+    <div v-if="showEditor">
+      <QuillEditor
+        class="editor-background"
+        :style="{ height: `${Height}px` }"
+        :content-type="'html'"
+        v-model:content="contents"
+        @update:content="updateContents"
+        theme="snow"
+        :options="editorOptions"
+        />
+        <button type="button" class="btn btn-primary" @click="savePetition">
+          Save changes
+        </button>
+        <button
+          type="button"
+          class="btn btn-secondary"
+          data-dismiss="modal"
+          @click="showEditor = false"
+        >
+          დახურვა
+        </button>
 
+    </div>
+  </div>
 </template>
-  
-<style>
-.dark .editor-background {
-    background-color: white;
-}
-
-.dark .container-background {
-    background-color: white;
-}
-
-</style>
 
 <script>
-import { QuillEditor } from '@vueup/vue-quill';
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { QuillEditor } from "@vueup/vue-quill";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 export default {
-    components: { QuillEditor },
-    props : {
-        Height: {
-        type: String
+  components: { QuillEditor },
+  props: {
+    Height: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      contents: "",
+      editorOptions: {
+        modules: {
+          toolbar: [
+            [{ header: [1, 2, 3, 4, 5, 6, false] }],
+            ["bold", "italic", "underline", "strike"],
+            [{ align: [] }],
+            [{ size: ["small", false, "large", "huge"] }],
+            [{ font: [] }],
+            ["clean"],
+          ],
+        },
+      },
+      showEditor: false,
+    };
+  },
+  methods: {
+    updateContents(value) {
+      if (value === "<p><br></p>") {
+        this.contents = "";
       }
+      this.$emit("update:content", this.contents);
     },
-    data() {
-        return {
-            contents: '',
-            editorOptions: {
-                modules: {
-                    toolbar: [
-                        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-                        ['bold', 'italic', 'underline', 'strike'],
-                        [{ 'align': [] }],
-                        [{ 'size': ['small', false, 'large', 'huge'] }],
-                        [{ 'font': [] }],
-                        ['clean'],
-                    ],
-                },
-            },
-        };
-    }, 
-    methods: {
-        updateContents(value) {
-            if (value === "<p><br></p>") {
-                this.contents = "";
-            }
-            this.$emit('update:content', this.contents);
-        }
-    },
+  },
 };
 </script>
-  
