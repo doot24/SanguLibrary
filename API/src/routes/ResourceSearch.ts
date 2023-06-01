174e914635e4fe98a5cb9d15df18066c3254d1f3
Akaki Khurtsidze <akakikhurtsidze77@gmail.com>
2023-06-02 01:20:37 +0400

    fix search

--- API/src/routes/ResourceSearch.ts
+++ API/src/routes/ResourceSearch.ts
@@ -1,4 +1,3 @@
-
 import { Router, Request, Response } from "express";
 const router = Router();
 
@@ -12,102 +11,32 @@ import { JournalSchema } from "../schemas/ResourceSchemas/Journal";
 import { DissertationSchema } from "../schemas/ResourceSchemas/Dissertation";
 import { RiderSchema } from "../schemas/ResourceSchemas/Rider";
 
-router.get('/resource/', IsAuthenticated, async (req: Request, res: Response) => {
+router.get('/resource/', IsAuthenticated, async (req, res) => {
+  const { text, page, pageSize }: any = req.query; // Get search term, page, and pageSize from query parameters
 
   try {
-    if (!req.query) {
-      throw new Error("no params provided");
+    const collections: any[] = [BookSchema, JournalSchema, DissertationSchema, RiderSchema];
+    const searchResults: SearchResults[] = [];
+
+    for (const schema of collections) {
+      const results = await schema.find({ $text: { $search: text } }).lean();
+      searchResults.push(...results);
     }
 
-    const { text } = req.query;
+    const totalResults = searchResults.length;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const paginatedResults = searchResults.slice(startIndex, endIndex);
 
-    const queryText = text?.toString() ?? '';
+    const pagination: Pagination = {
+      totalPages: Math.ceil(totalResults / pageSize),
+      currentPage : page,
+    };
 
-    let page = Number(req.query.page) || 1;
-    let pageSize = Number(req.query.pageSize) || 10;
-    let startIndex = (page - 1) * pageSize;
-
-    const results: SearchResults = new SearchResults();
-
-    const agg: any = [
-      {
-        $match: {
-          $text: { $search: String(queryText) }
-        }
-      },
-      {
-        $lookup: {
-          from: 'checkouts',
-          localField: '_id',
-          foreignField: 'resource_id',
-          as: 'checkout'
-        }
-      },
-      {
-        $match: {
-          $expr: {
-            $eq: [{ $size: '$checkout' }, 0]
-          }
-        }
-      },
-      {
-        $project: {
-          checkout: 0
-        }
-      },
-      {
-        $skip: startIndex
-      },
-      {
-        $limit: pageSize
-      }
-    ];
-
-
-    const bookPromise = BookSchema.aggregate(agg);
-    const journalPromise = JournalSchema.aggregate(agg);
-    const riderPromise = RiderSchema.aggregate(agg);
-    const dissertationPromise = DissertationSchema.aggregate(agg);
-
-    const countPromise = Promise.all([
-      BookSchema.countDocuments(agg),
-      JournalSchema.countDocuments(agg),
-      RiderSchema.countDocuments(agg),
-      DissertationSchema.countDocuments(agg),
-    ]);
-
-    const [books, journals, riders, dissertations] = await Promise.all([bookPromise, journalPromise, riderPromise, dissertationPromise]);
-    const [bookCount, journalCount, riderCount, dissertationCount] = await countPromise;
-
-    results.books.data = books;
-    results.books.amount = bookCount;
-
-    results.journals.data = journals;
-    results.journals.amount = journalCount;
-
-    results.riders.data = riders;
-    results.riders.amount = riderCount;
-
-    results.dissertations.data = dissertations;
-    results.dissertations.amount = dissertationCount;
-
-    // paginate results.
-    let totalAmount: number = (results.books.amount + results.journals.amount + results.riders.amount + results.dissertations.amount);
-
-    let paginationData: Pagination = new Pagination();
-    paginationData.currentPage = page;
-    paginationData.totalPages = Math.ceil(totalAmount / pageSize);
-
-    const final: any[] = [...results.books.data, ...results.journals.data, ...results.dissertations.data, ...results.riders.data];
-
-    res.status(200).json({ status: "success", paginationData, results: final });
-  }
-  catch (err) {
-    let paginationData: Pagination = new Pagination();
-    paginationData.currentPage = 1;
-    paginationData.totalPages = 1;
-
-    res.status(200).json({ status: "success", paginationData, results: [] });
+    res.status(200).json({ searchResults: paginatedResults, pagination });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while searching.' });
   }
 });
 
